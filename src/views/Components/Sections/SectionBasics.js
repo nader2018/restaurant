10d7a40462d1391583f6d60664ec0b28751262e3
nader <nader.hammami.2014@gmail.com>
2020-01-08 18:21:37 +0100

    first commit

---
+++ src/views/Components/Sections/SectionBasics.js
@@ -0,0 +1,119 @@
+import React from "react";
+// plugin that creates slider
+import Slider from "nouislider";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Radio from "@material-ui/core/Radio";
+import Switch from "@material-ui/core/Switch";
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+import People from "@material-ui/icons/People";
+import Check from "@material-ui/icons/Check";
+import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import CustomLinearProgress from "components/CustomLinearProgress/CustomLinearProgress.js";
+import Paginations from "components/Pagination/Pagination.js";
+
+import styles from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function SectionBasics() {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([24, 22]);
+  const [selectedEnabled, setSelectedEnabled] = React.useState("b");
+  const [checkedA, setCheckedA] = React.useState(true);
+  const [checkedB, setCheckedB] = React.useState(false);
+  React.useEffect(() => {
+    if (
+      !document
+        .getElementById("sliderRegular")
+        .classList.contains("noUi-target")
+    ) {
+      Slider.create(document.getElementById("sliderRegular"), {
+        start: [40],
+        connect: [true, false],
+        step: 1,
+        range: { min: 0, max: 100 }
+      });
+    }
+    if (
+      !document.getElementById("sliderDouble").classList.contains("noUi-target")
+    ) {
+      Slider.create(document.getElementById("sliderDouble"), {
+        start: [20, 60],
+        connect: [false, true, false],
+        step: 1,
+        range: { min: 0, max: 100 }
+      });
+    }
+    return function cleanup() {};
+  });
+  const handleToggle = value => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    setChecked(newChecked);
+  };
+  return (
+    <div className={classes.sections}>
+      <div className={classes.container}>
+        <div className={classes.title}>
+          <h2>Basic Elements</h2>
+        </div>
+        <div id="buttons">
+          <div className={classes.title}>
+            <h3>
+              Buttons
+              <br />
+              <small>Pick your style</small>
+            </h3>
+          </div>
+          
+          <div className={classes.title}>
+            
+          </div>
+          
+          <div className={classes.title}>
+           
+          </div>
+          
+        </div>
+        <div className={classes.space50} />
+        
+        <div className={classes.space70} />
+        
+        
+        <div id="sliders">
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <div className={classes.title}>
+                <h3>Sliders</h3>
+              </div>
+              <div id="sliderRegular" className="slider-primary" />
+              <br />
+              <div id="sliderDouble" className="slider-info" />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={6}>
+              <div className={classes.title}>
+                <h3>Badges</h3>
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/views/Components/Sections/SectionTypography.js
@@ -0,0 +1,126 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Small from "components/Typography/Small.js";
+import Danger from "components/Typography/Danger.js";
+import Warning from "components/Typography/Warning.js";
+import Success from "components/Typography/Success.js";
+import Info from "components/Typography/Info.js";
+import Primary from "components/Typography/Primary.js";
+import Muted from "components/Typography/Muted.js";
+import Quote from "components/Typography/Quote.js";
+
+import image1 from "assets/img/plat1.jpg";
+import image2 from "assets/img/plat2.jpeg";
+import image3 from "assets/img/plat3.jpg";
+import image4 from "assets/img/plat4.jpg";
+import image5 from "assets/img/plat5.jpg";
+import image6 from "assets/img/plat6.jpeg";
+
+import styles from "assets/jss/material-kit-react/views/componentsSections/typographyStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function SectionTypography() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <div className={classes.container}>
+        <div id="typography">
+          <div className={classes.title}>
+            <h2>Bienvenue</h2>
+          </div>
+          
+        </div>
+        <div className={classes.space50} />
+        <div id="images">
+          <div className={classes.title}>
+            <h2>Plats</h2>
+          </div>
+          <br />
+          <GridContainer>
+            <GridItem xs={12} sm={2}>
+              <h4>Plat1</h4>
+              <img
+                src={image1}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+            </GridItem>
+          
+            <GridItem xs={12} sm={2} className={classes.marginLeft}>
+              <h4>Plat2</h4>
+              <img
+                src={image3}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+            </GridItem>
+            <GridItem xs={12} sm={2} className={classes.marginLeft}>
+              <h4>Plat3</h4>
+              <img
+                src={image4}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+            </GridItem>
+            <GridItem xs={12} sm={2} className={classes.marginLeft}>
+              <h4>Plat4</h4>
+              <img
+                src={image5}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+            </GridItem>
+            <GridItem xs={12} sm={2} className={classes.marginLeft}>
+              <h4>Plat5</h4>
+              <img
+                src={image6}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+            </GridItem>
+          </GridContainer>
+          <GridContainer />
+        </div>
+        <div className={classes.space50} />
+      </div>
+    </div>
+  );
+}
